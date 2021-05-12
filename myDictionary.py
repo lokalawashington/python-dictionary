488291650ef0f6969b719ff9436780a657f4a500
lokalawashington <lokalawashington2019@gmail.com>
2021-05-12 09:39:27 -0400

    initial commit

---
+++ myDictionary.py
@@ -0,0 +1,8 @@
+programing_dictionary = {"BUG":"An error in a program that prevents the program from runing"
+                               "as expected.",
+                         "Function:":"A piece of code that you can easily call over and over again",
+                         "Loop":"An action of doing something over and over again"
+                         }
+bug = programing_dictionary["BUG"]
+print(programing_dictionary)
+print(bug)
